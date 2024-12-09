13a6322e94223eae08628a8d925b77e505ffee6e
Acylation <532117255@qq.com>
2024-12-09 04:15:38 -0500

    Remove template codes

--- src/renderer/src/App.svelte
+++ src/renderer/src/App.svelte
@@ -1,22 +1,9 @@
 <script lang="ts">
-  import './app.css';
-  import Versions from './components/Versions.svelte';
-  import electronLogo from './assets/electron.svg';
-
   const ipcHandle = (): void => window.electron.ipcRenderer.send('ping');
 
   import { Alert } from 'flowbite-svelte';
 </script>
 
-<img alt="logo" class="logo" src={electronLogo} />
-<div class="creator">Powered by electron-vite</div>
-<div class="text">
-  Build an Electron app with
-  <span class="svelte">Svelte</span>
-  and
-  <span class="ts">TypeScript</span>
-</div>
-<p class="tip">Please try pressing <code>F12</code> to open the devTool</p>
 <div class="actions">
   <div class="action">
     <a href="https://electron-vite.org/" target="_blank" rel="noreferrer">Documentation</a>
@@ -25,11 +12,4 @@
     <!-- svelte-ignore a11y-click-events-have-key-events a11y-no-static-element-interactions a11y-missing-attribute-->
     <a target="_blank" rel="noreferrer" on:click={ipcHandle}>Send IPC</a>
   </div>
-</div>
-<Versions />
-<div class="p-8">
-  <Alert>
-    <span class="font-medium">Info alert!</span>
-    Change a few things up and try submitting again.
-  </Alert>
 </div>
