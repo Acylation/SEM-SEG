0723018bdc847298a5b79ed7da1e3a5f1647f872
Acylation <532117255@qq.com>
2024-12-13 15:07:03 -0500

    Add Konva

---
+++ src/renderer/src/components/Konva.svelte
@@ -0,0 +1,9 @@
+<script>
+  import { Stage, Layer, Rect } from 'svelte-konva';
+</script>
+
+<Stage config={{ width: 200, height: 200 }}>
+  <Layer>
+    <Rect config={{ x: 100, y: 100, width: 200, height: 100, fill: 'blue' }} />
+  </Layer>
+</Stage>
